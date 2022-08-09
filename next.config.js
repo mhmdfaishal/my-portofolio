edf1b68ea2c52ecb66b31d61e1e9983dccaa2626
Muhammad Faishal Dienul Haq <mhmd.faishal123@gmail.com>
2022-08-09 21:53:56 +0700

    Edit basepath

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "static/images",
   },
-  basePath: "/mhmdfaishal",  
-  assetPrefix: "/mhmdfaishal",  
+  basePath: "/",  
+  assetPrefix: "/",  
 };
 
 module.exports = nextConfig;
