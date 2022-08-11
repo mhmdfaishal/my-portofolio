5a8ec15cdc48c4c7a39ece54c964bc2fecde432d
Muhammad Faishal Dienul Haq <mhmd.faishal123@gmail.com>
2022-08-11 11:23:23 +0700

    Delete aos on certificate:

--- components/Certificates.tsx
+++ components/Certificates.tsx
@@ -13,7 +13,7 @@ export const Certificates: NextPage<ICertificatesProps> = ({ certificates }) => 
         <h1 className="certificates_heading">Courses &amp; Certificates</h1>
         <div className="container">
             {certificates?.map(certificate => (
-                <div className="certificate" data-aos="fade-up" key={certificate?.uniqueId} onClick={() => window.open(certificate?.url, "_blank")}>
+                <div className="certificate" key={certificate?.uniqueId} onClick={() => window.open(certificate?.url, "_blank")}>
                     <img src={certificate?.image?.url} alt="" />
                     <h1>{certificate?.name}</h1>
                 </div>
