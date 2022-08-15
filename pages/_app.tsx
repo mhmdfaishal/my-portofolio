d69fd8592e2ff6f50dc944b2f53d1523f8bfa48b
Muhammad Faishal Dienul Haq <mhmd.faishal123@gmail.com>
2022-08-15 15:28:44 +0700

    Edit GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,13 +22,13 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-HFH09469DM`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
       />
       <Script id="google-analytics-script" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];
            function gtag(){dataLayer.push(arguments);}
            gtag('js', new Date());
-           gtag('config', 'G-HFH09469DM');`}
+           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');`}
       </Script>
       <Component {...pageProps} />
     </>
