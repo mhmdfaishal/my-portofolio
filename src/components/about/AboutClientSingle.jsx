54114457000e2dbcfbf6a2d1056ec4d61dd7f4a3
Muhammad Faishal Dienul Haq <mhmd.faishal123@gmail.com>
2023-08-12 18:28:23 +0700

    chore: change profile image and clients height

--- src/components/about/AboutClientSingle.jsx
+++ src/components/about/AboutClientSingle.jsx
@@ -4,7 +4,7 @@ const AboutClientSingle = ({ title, image }) => {
 		<div className="border items-center flex justify-center text-center bg-ternary-teal-glass border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer">
 			<img
 				src={image}
-				className="w-40"
+				className={title === "Campuspedia Indonesia" || title === "Ibrahim Ulul Azmi"? "py-13 w-40" :"w-40"}
 				alt={title}
 			/>
 		</div>
--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -1,4 +1,4 @@
-import profileImage from '../../images/profile.jpg';
+import profileImage from '../../images/profile.png';
 import { useContext } from 'react';
 import AboutMeContext from '../../context/AboutMeContext';
 
@@ -8,7 +8,7 @@ const AboutMeBio = () => {
 	return (
 		<div className="block sm:flex sm:gap-10 mt-10 sm:mt-20 items-center">
 			<div className="w-full sm:w-1/4 mb-7 sm:mb-0">
-				<img src={profileImage} className="rounded-full w-96" alt="" />
+				<img src={profileImage} className="w-96" alt="" />
 			</div>
 
 			<div className="font-general-regular w-full sm:w-3/4 text-justify">
